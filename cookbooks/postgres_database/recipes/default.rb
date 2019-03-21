86929d155a25959116e367e21e6aff836ceca8ac
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-03-21 16:03:40 +0300

    feat(postgres): install postgres on ubuntu
    - update cache on ubuntu xenial
    - ensure wget is installed
    - ensure ca-certificates package is installed
    - add signing key for Ubuntu
    - Add the source list for xenial machine
    - Update the package lists
    - Install postgresql version 10 and its client
    
    [ Fixes #164797029]

--- cookbooks/postgres_database/recipes/default.rb
+++ cookbooks/postgres_database/recipes/default.rb
@@ -6,3 +6,28 @@
 
 # Set up the apt_update command to be running updates when the chef client run is called
 # First step to setting up postgres is updating the apt cache
+
+apt_update 'Update the apt cache on chef client runs' do
+    action :update
+  end
+  
+package 'wget'
+package 'ca-certificates'
+
+execute 'Import the repository signing key' do
+  command 'wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -'
+  action :run
+end
+
+file '/etc/apt/sources.list.d/pgdg.list' do
+  content 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main'
+end
+
+apt_update 'Update the package lists' do
+  action :update
+end
+
+# Install core database server postgresql version 10
+package 'postgresql-10'
+# Install client libraries and binaries for postgresql version 10
+package 'postgresql-client-10'
---
+++ docs/setup.md
@@ -0,0 +1,6 @@
+### Generate a cookbook for this repository
+
+On the root of the repository run the command below:
+```
+chef generate cookbook cookbooks/{name of the cookbook}
+```
