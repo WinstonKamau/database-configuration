9b334a9b337280990c51854d612d37eeffabf3f7
WinstonKamau <29925144+WinstonKamau@users.noreply.github.com>
2019-06-08 04:15:37 +0300

    feat(infra): setup the chef infra server
    - create a compute engine for chef infra server
    - setup firewall rules for port 80 and port 443
    - make the project portable by requiring variables
    - ignore platform specific and secrets on git
    
    [Fixes #166492044]

---
+++ chef-server-configuration/variables.tf
@@ -0,0 +1,23 @@
+variable "hostname" {
+  
+}
+variable "project" {
+  
+}
+variable "chef_server_zone" {
+  
+}
+variable "chef_server_name" {
+  
+}
+variable "chef_ip_address" {
+  
+}
+
+variable "region" {
+  
+}
+
+variable "machine_type" {
+  
+}
